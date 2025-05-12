6d87dcdaa9b1c82a8369bf5a9b2f06edf39b6faa
Raul Lima Alves <raullalves@gmail.com>
2025-05-12 15:37:24 -0300

    simplify interface

--- binomial_tree_finance.py
+++ binomial_tree_finance.py
@@ -16,12 +16,12 @@ def compute_option_price_given_option_side(price, exercise_price, option_side):
         return 0 if price < exercise_price else price - exercise_price
 
     raise Exception(f"option type {option_type} not implemented")
+
 def _update_option_price(nodes, exercise_price, option_side):
     for node in nodes:
         node.option_price = compute_option_price_given_option_side(price=node.price,
                                                                    exercise_price=exercise_price,
                                                                    option_side=option_side)
-        print(f"Option price is {node.option_price} for price {node.price}")
 
 def traverse_populate(price, vol, num_steps, time_to_expire, exercise_price, option_side):
     root = Node(price=price)
@@ -30,8 +30,6 @@ def traverse_populate(price, vol, num_steps, time_to_expire, exercise_price, opt
     dt = time_to_expire/num_steps
     price_factor_neg = np.exp(-vol*np.sqrt(dt))
     price_factor_pos = 1/price_factor_neg
-    #price_factor_neg = 1-vol
-    #price_factor_pos = (1+vol)
     while curr_level:
         next_level = []
         for node in curr_level:
@@ -41,7 +39,6 @@ def traverse_populate(price, vol, num_steps, time_to_expire, exercise_price, opt
             next_level.append(node.right)
 
         curr_step += 1
-        print(f"im at step {curr_step}")
         if curr_step >= num_steps:
             _update_option_price(nodes=[n.left for n in curr_level] + [n.right for n in curr_level], exercise_price=exercise_price, option_side=option_side)
             break
@@ -52,16 +49,10 @@ def traverse_populate(price, vol, num_steps, time_to_expire, exercise_price, opt
 def compute_price(root, p, discount, option_type, exercise_price, option_side):
 
     if root:
-
-        # First recur on left child
         compute_price(root.left, p=p, discount=discount, option_type=option_type, exercise_price=exercise_price,
                       option_side=option_side)
-
-        # the recur on right child
         compute_price(root.right, p=p, discount=discount, option_type=option_type, exercise_price=exercise_price,
                       option_side=option_side)
-
-        # now print the data of node
         if root.option_price is None:
             opt_pricing_eq = discount * (p * root.left.option_price + (1-p) * root.right.option_price)
             option_result = compute_option_price_given_option_side(price=root.price,
@@ -72,18 +63,7 @@ def compute_price(root, p, discount, option_type, exercise_price, option_side):
             else:
                 root.option_price = max(opt_pricing_eq, option_result)
 
-            print(f"Option price {root.option_price} for price {root.price}")
-
-if __name__ == '__main__':
-    time_to_expire = 2
-    num_steps = 15
-    vol = 0.3
-    risk_free = 0.05
-    price = 50
-    exercise_price = 52
-    option_side = 'put'
-    option_type = 'american'
-
+def compute(price, vol, num_steps, time_to_expire, exercise_price, option_side):
     root = traverse_populate(price=price, vol=vol, num_steps=num_steps, time_to_expire=time_to_expire, exercise_price=exercise_price, option_side=option_side)
     dt = time_to_expire/num_steps
     price_factor_neg = np.exp(-vol*np.sqrt(dt))
@@ -93,4 +73,19 @@ if __name__ == '__main__':
     compute_price(root, p=p, discount=discount, option_type=option_type, exercise_price=exercise_price,
                   option_side=option_side)
 
-    print(root.option_price)
+    return root.option_price
+
+if __name__ == '__main__':
+    time_to_expire_yrs = 2
+    num_steps = 2
+    vol = 0.3
+    risk_free = 0.05
+    price = 50
+    exercise_price = 52
+
+    option_side = 'put'
+    option_type = 'american'
+
+    option_price = compute(price=price, vol=vol, num_steps=num_steps, time_to_expire=time_to_expire_yrs,
+                           exercise_price=exercise_price, option_side=option_side)
+    print(option_price)
--- binomial_tree_finance/options/base.py
+++
@@ -1,8 +0,0 @@
-class Base:
-    def __init__(self, price, number_of_steps, exercise_price, **kwargs):
-        self.number_of_steps = number_of_steps
-        self.price = price
-        self.exercise_price = exercise_price
-
-    def calculate(self):
-        pass
