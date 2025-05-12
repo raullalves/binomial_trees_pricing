d28e9c2d26f7796f065483798428ae2f0029c696
Raul Lima Alves <raullalves@gmail.com>
2025-05-12 15:27:38 -0300

    bugfixing opt compute with vol

--- binomial_tree_finance/utils/tree.py
+++ binomial_tree_finance/utils/tree.py
@@ -28,10 +28,10 @@ def traverse_populate(price, vol, num_steps, time_to_expire, exercise_price, opt
     curr_level = [root]
     curr_step = 0
     dt = time_to_expire/num_steps
-    #price_factor_neg = np.exp(-vol*np.sqrt(dt))
-    #price_factor_pos = 1/price_factor_neg
-    price_factor_neg = 1-vol
-    price_factor_pos = (1+vol)
+    price_factor_neg = np.exp(-vol*np.sqrt(dt))
+    price_factor_pos = 1/price_factor_neg
+    #price_factor_neg = 1-vol
+    #price_factor_pos = (1+vol)
     while curr_level:
         next_level = []
         for node in curr_level:
@@ -78,8 +78,8 @@ def compute_price(root, p, discount, option_type, exercise_price, option_side):
 
 if __name__ == '__main__':
     time_to_expire = 2
-    num_steps = 2
-    vol = 0.2
+    num_steps = 5
+    vol = 0.3
     risk_free = 0.05
     price = 50
     exercise_price = 52
@@ -89,10 +89,10 @@ if __name__ == '__main__':
     #traverse_populate(price=50, vol=0.3, num_steps=2, time_to_expire=2, exercise_price=52, option_type='put')
     root = traverse_populate(price=price, vol=vol, num_steps=num_steps, time_to_expire=time_to_expire, exercise_price=exercise_price, option_side=option_side)
     dt = time_to_expire/num_steps
-    #price_factor_neg = np.exp(-vol*np.sqrt(dt))
-    #price_factor_pos = 1/price_factor_neg
-    price_factor_neg = 1-vol
-    price_factor_pos = (1+vol)
+    price_factor_neg = np.exp(-vol*np.sqrt(dt))
+    price_factor_pos = 1/price_factor_neg
+    #price_factor_neg = 1-vol
+    #price_factor_pos = (1+vol)
     discount = np.exp(-risk_free * dt)
     p = (1/discount - price_factor_neg) / (price_factor_pos - price_factor_neg)
     compute_price(root, p=p, discount=discount, option_type=option_type, exercise_price=exercise_price,
